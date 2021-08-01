814be3bef08f8b5fa8c079d38a3e2c75c5c160b1
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-01 21:56:31 +0545

    Implemented test case for login feature using POM

---
+++ cypress/integration/test_loginPage.ts
@@ -0,0 +1,14 @@
+import { data } from "cypress/types/jquery"
+import {LoginPage} from "../pages/loginpage"
+
+const loginPage = new LoginPage()
+
+it('test_login', () => {
+    
+    cy.fixture('loginpage_data').then((data) => {
+        loginPage.navigate(data.url);
+        loginPage.enterUsername(data.username);
+        loginPage.enterPassword(data.password);
+        loginPage.clickLogin();
+    })
+})
