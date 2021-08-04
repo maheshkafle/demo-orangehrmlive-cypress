a8673d77ba032f2da0cff9a491dbb27528625bec
kaflemahesh <mahesh.kafle@smarsh.com>
2021-08-04 21:57:54 +0545

    Renamed test file

--- cypress/integration/test_OpenSourceOrangeHRM.ts
+++ cypress/integration/test_OpenSourceOrangeHRM.ts
@@ -4,10 +4,12 @@ import {LoginPage} from "../pages/loginpage"
 const loginPage = new LoginPage()
 
 it('test_login', () => {
+
     cy.fixture('test_data').then((data) => {
         loginPage.navigate(data.url);
         loginPage.enterUsername(data.username);
         loginPage.enterPassword(data.password);
         loginPage.clickLogin();
     })
+
 })
